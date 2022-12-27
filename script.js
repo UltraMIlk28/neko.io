0b8ee3b6cf145fe366355a92a51e22dc88e2eaf3
Fid <84630194+UltraMIlk28@users.noreply.github.com>
2022-12-28 02:28:45 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.open = function() {};
- window.print = function() {};
- if (false) {
-   window.ontouchstart = function() {};
- }
-if(document.location.search.match(/type=embed/gi)) {
-  window.parent.postMessage('resize', "*");
-}
